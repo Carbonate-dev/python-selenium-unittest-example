7b394ea79685a77b5a1fdaaf2aa0ff0577ca4243
Lewis Wright <LewisW@users.noreply.github.com>
2023-06-24 23:40:07 +0100

    Change to match docs

--- test/test_example.py
+++ test/test_example.py
@@ -1,6 +1,6 @@
 import os
 import unittest
-import carbonate_sdk as carbonate
+import carbonate_sdk
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
@@ -8,41 +8,39 @@ from selenium.webdriver.support.ui import Select
 
 class example_test(unittest.TestCase):
     browser = None
+    carbonate = None
 
     @classmethod
     def setUpClass(cls) -> None:
         # This can be any browser supported by webdriver
         driver = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()))
-        cls.browser = carbonate.WebDriver(driver)
-
-    def setUp(self):
-        self.carbonate_sdk = carbonate.SDK(
-            browser=self.browser,
+        cls.browser = carbonate_sdk.WebDriver(driver)
+        cls.carbonate = carbonate_sdk.SDK(
+            browser=cls.browser,
             cache_dir=os.path.join(os.path.splitext(__file__)[0]),
             # api_user_id="<your user ID>",  # TODO: Change me
             # api_key="<your api key>",  # TODO: Change me
         )
 
-    @carbonate.test()
+    @carbonate_sdk.test("carbonate")
     def test_select_birthday_from_the_dropdown(self):
-        self.carbonate_sdk.load(
+        self.carbonate.load(
             'https://carbonate.dev/demo-form'
         )
 
-        self.carbonate_sdk.action('select Birthday from the event type dropdown')
+        self.carbonate.action('select Birthday from the event type dropdown')
 
         self.assertTrue(
-            self.carbonate_sdk.assertion('the event type dropdown should be set to Birthday')
+            self.carbonate.assertion('the event type dropdown should be set to Birthday')
         )
 
-    @carbonate.test()
+    @carbonate_sdk.test("carbonate")
     def test_select_birthday_from_the_dropdown_advanced(self):
-        self.carbonate_sdk.load(
+        self.carbonate.load(
             'https://carbonate.dev/demo-form'
         )
 
-        select = self.carbonate_sdk.lookup('the dropdown')
+        select = self.carbonate.lookup('the event type dropdown')
         Select(select).select_by_visible_text('Birthday')
 
-        select = self.carbonate_sdk.lookup('the dropdown')
         self.assertEqual('Birthday', Select(select).first_selected_option.text)
